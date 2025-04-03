b2d6dfb5f5a7d61ba6b27370a75d297ab3bf14e8
mtayyab829 <muhammadtayyab2928@gmail.com>
2025-04-03 12:29:59 +0800

    setup.sh

--- Wix-Project-for-morgan-main/setup.sh
+++ Wix-Project-for-morgan-main/setup.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
+set -e
+
+# Install system dependencies
 apt-get update
-apt-get install -y wget chromium chromium-driver
+apt-get install -y \
+    wget \
+    chromium \
+    chromium-driver \
+    fonts-liberation \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libatspi2.0-0 \
+    libcairo2 \
+    libcups2 \
+    libdbus-1-3 \
+    libdrm2 \
+    libgbm1 \
+    libgtk-3-0 \
+    libnspr4 \
+    libnss3 \
+    libpango-1.0-0 \
+    libx11-6 \
+    libxcb1 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxrandr2 \
+    libxshmfence1
+
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Symlink chromium to google-chrome
+ln -s /usr/bin/chromium /usr/bin/google-chrome
